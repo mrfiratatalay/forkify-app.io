2ca412fe58e2ac17fec51e0c91b6f3ffc98f53ec
FIRAT ATALAY <atafiratbey@gmail.com>
2023-09-14 21:28:20 +0300

    The Forkify App

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -5,7 +5,7 @@ import icons from 'url:../../img/icons.svg'; // Parcel 2
 
 /* NOTE: The Fractional package has been reported to cause an error when deployed to a server. I suggest you to use Fracty instead. */
 // import { Fraction } from 'fractional';
-import fracty from "fracty";
+import fracty from 'fracty';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -131,7 +131,7 @@ class RecipeView extends View {
         <use href="${icons}#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
-      ing.quantity ? fracty(ing.quantity).toString() : ''
+        ing.quantity ? fracty(ing.quantity).toString() : ''
       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
